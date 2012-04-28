5ffe9292c72c7e9436cbd3627fd89a99c259f40d
S Anand <root.node@gmail.com>
2012-04-28 10:37:27 +0530

    Restructured code

--- index.php
+++ index.php
@@ -4,25 +4,21 @@
  * Run: index.php?userid=ID
  *
  */
+require_once 'simple_html_dom.php';
+
 $userid= trim($_GET['userid']);
-/* update your path accordingly */
-include_once 'simple_html_dom.php';
-$url = "http://pinterest.com/".$userid."/";
+$url = "http://pinterest.com/$userid/";
 $html = file_get_html($url);
-$pinboard =  $html->find('div[class=pin pinBoard]');
-$json=array();
-$i=0;
-$rs=array();
-foreach($pinboard as $pin){
-	$boardnames = $pin->find('a');
-	foreach($boardnames as $boardname){
-    	//echo $a->plaintext. "<br>";
-    	if($boardname->plaintext!='&nbsp;'){
-    	$rs[$i++]=$boardname->plaintext;
-    	}
+$rs = array();
+foreach($html->find('div[class=pinBoard]') as $pin){
+    foreach($pin->find('h3') as $boardname){
+        $rs[] = array(
+            $boardname->plaintext,
+            $boardname->find('a', 0)->href
+        );
     }
 }
-$json=array("boards"=>$rs);
-echo json_encode($json);
 
+header('Content-type: application/json');
+echo json_encode($rs);
 ?>
