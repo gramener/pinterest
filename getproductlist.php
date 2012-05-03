5104316c0ae9b993fe9e1f3a887640e88696e164
gramener <s.anand@gramener.com>
2012-05-03 20:04:37 +0530

    direct commit

--- getproductlist.php
+++ getproductlist.php
@@ -1,11 +1,31 @@
 <?php
 /*
 * author: Jayaseelan Gabriel
-* Run: index.php?userid=ID
-*
+* Run: getproductlist.php?userid=ID&boardname=BOARDNAME
+*example: http://localhost/<appname>/getproductlist.php?userid=rockey_nebhwani&boardname=value-clocks
 */
 require_once 'simple_html_dom.php';
+$userid= trim($_GET['userid']);
+$boardname=trim($_GET['boardname']);
+$url = "http://pinterest.com/$userid/$boardname/";
+$html = file_get_html($url);
+$rs = array();
+$productName='';
+$productURL='';
+foreach($html->find('div[class=pin]') as $pin){
+  foreach($pin->find('p[class=description]') as $desc){
+		$productName=$desc->plaintext;
+	}
+	foreach($pin->find('p[class=NoImage]') as $pinid){
+		foreach($pinid->find('a') as $p){
+		 $productURL=$p->href;
+		}
+	}
+	$rs[]=array($productName,$productURL);
+}
 
+header('Content-type: application/json');
+echo json_encode($rs);
 
 
 
